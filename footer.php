ee60c050c053bf890c6722750c1f376fa498d8de
Cedric Chin <cedric@elijames.org>
2014-03-17 00:48:29 +0800

    Footer styles.
    - Much to do here. Leaving it simple and blank 
    for the time being.

--- footer.php
+++ footer.php
@@ -10,15 +10,24 @@
 
 	</div><!-- #content -->
 
+<footer id="footer-container"> 
   <div class="row">
-	<footer id="colophon" class="large-12 columns" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'treehouse' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'treehouse' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'treehouse' ), 'treehouse', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <div class="small-12 large-6 columns text-right" role="navigation">
+       <ul class="inline-list"> 
+        <li>Follow us:</li>
+        <li><a href="<?php bloginfo('rss2_url'); ?>">RSS</a></li> 
+        <li><a href="http://tembusu.nus.edu.sg/">Tembusu College</a></li> 
+        <li><a href="http://instagram.com/tembusucollege">Instagram</a></li>
+        <li><a href="https://www.youtube.com/user/TembusuCollege">Youtube</a>
+      </ul>
+    </div>
   </div>
+  <div class="row">
+	<div id="colophon" class="large-12 columns" role="contentinfo">
+    &copy; Tembusu College &amp; Our Authors
+	</div><!-- #colophon -->
+  </div><!--row-->
+</footer><!--footer-container-->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
