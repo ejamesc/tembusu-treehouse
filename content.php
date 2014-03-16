d6760f3aa3630fb3b8f7ec59b83c4f5045c6e441
Cedric Chin <cedric@elijames.org>
2014-03-17 00:06:05 +0800

    Finalise block layout for stories in grid.

--- content.php
+++ content.php
@@ -5,15 +5,26 @@
 ?>
 <li> <!--needed for foundation's block grid-->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+  <header class="entry-header">
+<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ' / ', 'treehouse' ) );
+				if ( $categories_list && treehouse_categorized_blog() ) :
+			?>
+			<span class="cat-links">
+				<?php printf( __( '%1$s', 'treehouse' ), $categories_list ); ?>
+			</span>
+			<?php endif; // End if categories ?>
+		<?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php treehouse_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    <?php if( has_post_thumbnail() ) : ?>
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?></a>
+    <?php endif ?>
+
+		
+			</header><!-- .entry-header -->
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
@@ -21,27 +32,14 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-summary">
-    <?php if( has_post_thumbnail() ) : ?>
-      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?></a>
-    <?php endif ?>
     <?php the_excerpt(); ?>
-    <a href="<?php the_permalink(); ?>">Read more</a>
+    
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'treehouse' ) );
-				if ( $categories_list && treehouse_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'treehouse' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
+  <footer class="entry-footer">
+  <a href="<?php the_permalink(); ?>">Read more &rarr;</a>
+		
 		<?php // edit_post_link( __( 'Edit', 'treehouse' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
  * @package treehouse
  */
 ?>
-	<div id="secondary" class="small-12 large-3 columns" role="complementary">
+	<div id="secondary" class="small-12 large-2 large-offset-1 columns" role="complementary">
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 			<aside id="search" class="widget widget_search">
@@ -19,14 +19,6 @@
 				</ul>
 			</aside>
 
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', '_s' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
 
 		<?php endif; // end sidebar widget area ?>
 	</div><!-- #secondary -->
