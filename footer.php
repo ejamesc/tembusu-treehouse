7bb9fee79ef9eff42340929877214971e0f41548
Cedric Chin <cedric@elijames.org>
2014-03-28 01:31:42 +0800

    Updated footer styles, fixed CSS bugs.

--- footer.php
+++ footer.php
@@ -12,9 +12,20 @@
 
 <footer id="footer-container"> 
   <div class="row">
-    <div class="small-12 large-6 columns text-right" role="navigation">
-       <ul class="inline-list"> 
-        <li>Follow us:</li>
+    <div class="small-12 large-4 columns" role="navigation">
+      <h3 class="footer-heading">Navigation</h3>
+      <?php wp_nav_menu( array( 'theme_location' => 'primary', 
+                                'container' => false,
+                                'menu_class' => 'footer-nav-list' ) ); ?>
+ 
+    </div>
+    <div class="small-12 large-4 columns">
+      <h3 class="footer-heading">Categories</h3>
+      <ul><?php wp_list_categories( array('title_li' => null ) ); ?></ul>
+    </div>
+    <div class="small-12 large-4 columns">
+        <h3 class="footer-heading">Follow us</h3>
+       <ul> 
         <li><a href="<?php bloginfo('rss2_url'); ?>">RSS</a></li> 
         <li><a href="http://tembusu.nus.edu.sg/">Tembusu College</a></li> 
         <li><a href="http://instagram.com/tembusucollege">Instagram</a></li>
