b235795d707087a05e248c2a65010da487508c16
Cedric Chin <cedric@elijames.org>
2014-04-03 17:42:01 +0800

    Change header to have navbar and logo same level.

--- header.php
+++ header.php
@@ -29,19 +29,20 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<header class="row" role="banner">
-      <div class="text-center masthead">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-          <img alt="Treehouse, A Tembusu Publication" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" title="Treehouse, A Tembusu Publication" />
-        <h1 class="wordmark">Treehouse</h1>
-        </a>
-      </div>
+	<header class="row masthead" role="banner">
+        <div class="small-12 large-4 columns">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img alt="Treehouse, A Tembusu Publication" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" title="Treehouse, A Tembusu Publication" />
+            <h1 class="wordmark">Treehouse</h1>
+          </a>
+        </div><!-- small-12 large-4 columns -->
 
-		<nav id="navbar" class="large-12 columns" role="navigation">
-      <?php wp_nav_menu( array( 'theme_location' => 'primary', 
+        <nav id="navbar" class="small-12 large-8 columns" role="navigation">
+          <?php wp_nav_menu( array( 'theme_location' => 'primary', 
                                 'container' => false,
                                 'menu_class' => 'nav-list' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		    </nav><!-- #site-navigation -->
+      </div><!-- masthead -->
 
+	</header>
 	<div id="content" class="site-content">
