1ac210da9a3493ae0af5f25fad6bec3b2d34f34f
Cedric Chin <cedric@elijames.org>
2014-04-05 22:55:53 +0800

    Fix search bar and sidebar top margin.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,4 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
 		<input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:" />
-	</label>
-	<input type="submit" class="search-submit button tiny green" value="Search" />
+	<input type="submit" class="search-submit button tiny green" value="&rarr;" />
 </form>
