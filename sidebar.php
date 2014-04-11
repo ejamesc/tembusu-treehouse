90a46dadc19d0577a68672b6b2ea50bb4d71d18b
Cedric Chin <cedric@elijames.org>
2014-04-12 03:06:09 +0800

    Updates to reflect sidebar styles.

--- sidebar.php
+++ sidebar.php
@@ -6,13 +6,6 @@
  */
 ?>
 <div id="secondary" class="<?php if(is_home()) { echo "top-offset";} ?> small-12 large-2 columns" role="complementary">
-    <aside class="widget">
-      <ul class="social-widget">
-        <li><a href="http://instagram.com/tembusucollege"><img class="social" src="<?php echo get_template_directory_uri() ?>/img/instagram.png" width=60 />&nbsp;&nbsp;  Instagram</a></li>
-        <li><a href="https://www.youtube.com/user/TembusuCollege"><img class="social" src="<?php echo get_template_directory_uri() ?>/img/youtube.png" width=60/>&nbsp;&nbsp;  Youtube</a></li>
-      </ul>
-    </aside>
-
       <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 			<aside id="search" class="widget widget_search">
